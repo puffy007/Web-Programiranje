2a46722829ba09c19fac1d8fb71887c9655d7d48
puffy007 <romanahorvat007@gmail.com>
2026-04-08 16:08:36 +0200

    Zadatak 3

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const fs = require('fs');
+const path = require('path');
+
+app.set("view engine", "ejs");
 app.use(express.static('public'));
-app.get('/', (req, res) => {
-    res.send('Pozdrav sa Railway servera!');
+app.get('/slike', (req, res) => {
+    const dataPath = path.join(__dirname, 'images.json');
+    const images = JSON.parse(fs.readFileSync(dataPath, "utf8"));
+
+    res.render('slike', { images });
 });
 app.listen(PORT, () => {
     console.log('Server pokrenut na portu ${PORT}');
